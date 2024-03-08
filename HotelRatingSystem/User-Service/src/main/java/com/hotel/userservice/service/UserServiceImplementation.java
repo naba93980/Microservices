239f5b4157f8f31202c84b3fd39a578b2560d5a1
naba93980 <72063612+naba93980@users.noreply.github.com>
2024-03-08 22:58:28 +0530

    UserService gets ratings and foreach rating gets the hotel

--- HotelRatingSystem/User-Service/src/main/java/com/hotel/userservice/service/UserServiceImplementation.java
+++ HotelRatingSystem/User-Service/src/main/java/com/hotel/userservice/service/UserServiceImplementation.java
@@ -1,10 +1,19 @@
 package com.hotel.userservice.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.hotel.userservice.entities.Hotel;
+import com.hotel.userservice.entities.Rating;
 import com.hotel.userservice.entities.User;
 import com.hotel.userservice.error.ResourceNotFoundException;
 import com.hotel.userservice.error.SomethingWentWrongException;
@@ -20,10 +29,12 @@ public class UserServiceImplementation implements UserService {
 
   UserRepository userRepository;
 
+  RestTemplate restTemplate;
+
   @Override
   public User createUser(User user) {
     try {
-       return userRepository.save(user);
+      return userRepository.save(user);
     } catch (RuntimeException e) {
       throw new SomethingWentWrongException("Something went wrong, could not create");
     }
@@ -31,13 +42,27 @@ public class UserServiceImplementation implements UserService {
 
   @Override
   public User getUser(Long userId) {
-    return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("No such User found"));
+    User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("No such User found"));
+    ResponseEntity<List<Rating>> response = restTemplate.exchange("http://rating-service/ratings/getRatingByUserId?userId=" + userId, HttpMethod.GET, null, new ParameterizedTypeReference<List<Rating>>() {});
+    List<Rating> ratings = response.getBody();
+    user.setRatings(ratings);
+    return user;
   }
 
   @Override
   public List<User> getAllUsers() {
     try {
-      return userRepository.findAll();
+      return userRepository.findAll().stream().map(user -> {
+        ResponseEntity<List<Rating>> response = restTemplate.exchange("http://rating-service/ratings/getRatingByUserId?userId=" + user.getUserId(), HttpMethod.GET, null, new ParameterizedTypeReference<List<Rating>>() {});
+        List<Rating> ratings = response.getBody();
+          ratings = ratings.stream().map(rating -> {
+            Hotel hotel = restTemplate.getForObject("http://hotel-service/hotels/getHotel?hotelId=" + rating.getHotelId(), Hotel.class);
+            rating.setHotel(hotel);
+            return rating;
+          }).collect(Collectors.toList());
+        user.setRatings(ratings);
+        return user;
+      }).collect(Collectors.toList());
     } catch (RuntimeException e) {
       throw new ResourceNotFoundException("Something went wrong, could not fetch");
     }
@@ -45,7 +70,8 @@ public class UserServiceImplementation implements UserService {
 
   @Override
   public User updateUser(User user) {
-    User existingUser = userRepository.findById(user.getUserId()).orElseThrow(() -> new ResourceNotFoundException("No such User found"));
+    User existingUser = userRepository.findById(user.getUserId())
+        .orElseThrow(() -> new ResourceNotFoundException("No such User found"));
     try {
       BeanUtils.copyProperties(user, existingUser);
       return userRepository.save(existingUser);
